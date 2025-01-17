fa8f3043197fba2cf3231854c7bf30fd222ff41d
Rajganez <ganezsouth@live.com>
2025-01-17 19:28:18 +0530

    Added frontend deployed url for cors

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ await connectToDB;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://workotaskbyraj.netlify.app/"],
     // credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     // allowedHeaders: ["Content-Type", "Authorization"],
